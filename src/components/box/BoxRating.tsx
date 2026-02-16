b979121796fbdfd265226f9af47fecfe00dc8736
ibour <ianbour2016@gmail.com>
2026-02-16 10:15:34 +0100

    Added basic voting letters

--- src/components/box/BoxRating.tsx
+++ src/components/box/BoxRating.tsx
@@ -2,9 +2,18 @@ import { BoxRatingProps } from "@/types/props/box/BoxRatingProps";
 
 const BoxRating = (props: BoxRatingProps) => {
 
-    return (
-        <div className="flex h-[12vh] aspect-square items-center justify-center bg-neutral-dark border-6 border-neutral-light rounded-3xl">
+    const letters = [
+        (<p className="text-gray-400">X</p>),
+        (<p className="text-red-400">D</p>),
+        (<p className="text-blue-400">C</p>),
+        (<p className="text-purple-400">B</p>),
+        (<p className="text-amber-400">A</p>),
+        (<p className="text-blue-100">S</p>),
+    ];
 
+    return (
+        <div className="flex h-[12vh] aspect-square text-8xl font-kanit font-bold italic items-center justify-center bg-neutral-dark border-6 border-neutral-light rounded-3xl">
+            {letters[props.rating]}
         </div>
     );
 
